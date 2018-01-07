f5fc88a4b8dbbed6add33c717002ba56e8c23af6
Padmanathan Rajaram (Contractor) <Rajaram.Padmanathan01@us.mcd.com>
2018-01-07 09:10:56 -0600

    Initial Commit

---
+++ src/main/java/com/prajaram/zuul/ZuulServerApplication.java
@@ -0,0 +1,16 @@
+package com.prajaram.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableZuulProxy
+@RestController
+public class ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+}
